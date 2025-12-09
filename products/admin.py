402b823becec1349962c0c506929aa5c378adb1f
foster95 <alicerosefoster95@gmail.com>
2025-12-09 20:59:23 +0000

    customised admin

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,25 @@ from .models import Product, Category
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    """Admin model for Product to display relevant fields."""
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image'
+    )
 
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Admin model for Category to display relevant fields."""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Category(models.Model):
     """Model representing a product category."""
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,7 +16,7 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
     
-    
+
 class Product(models.Model):
     """Model representing a product."""
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
