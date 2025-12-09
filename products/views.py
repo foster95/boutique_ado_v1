c5f3b78ed50709acbc69d319cc07999ff423a1a2
foster95 <alicerosefoster95@gmail.com>
2025-12-09 22:13:19 +0000

    added product details functionality

--- products/views.py
+++ products/views.py
@@ -5,10 +5,20 @@ from .models import Product
 
 def all_products(request):
     """A view to show all products page."""
-
     products = Product.objects.all()
+
     context = {
         'products': products,
     }
     
     return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """A view to show individual product details."""
+    product = Product.objects.get(pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
