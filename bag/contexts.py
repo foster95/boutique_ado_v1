b91e2002db82df9c1aa3e7ca504abeab0f4d347f
foster95 <alicerosefoster95@gmail.com>
2026-01-02 15:15:53 +0000

    finished product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
 
     bag_items = []
@@ -22,7 +21,7 @@ def bag_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-
+            
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
@@ -30,7 +29,7 @@ def bag_contents(request):
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
-                    'quantity': quantity,
+                    'quantity': item_data,
                     'product': product,
                     'size': size,
                 })
@@ -41,9 +40,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
