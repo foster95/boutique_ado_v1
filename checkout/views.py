2885afc733f7a4c39ebf763bbd02f92bbd3f5746
foster95 <alicerosefoster95@gmail.com>
2026-01-04 18:51:25 +0000

    added stipe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51SlvuwCToKLU8rrOujL3XkBcCd9jkdoa5uRx8Z70K8AyaPNcWIf6XqKChwzHNptWBIzE7lslbry6RkAZs0usVJ9P00zqcjQnpI',
+        'client_secret': 'test_client',
     }
 
     return render(request, template, context)
