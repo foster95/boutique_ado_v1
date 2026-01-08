a6ec88db296fb23ba69252a2b48182ac3acd4aa4
foster95 <alicerosefoster95@gmail.com>
2026-01-08 15:42:03 +0000

    finished add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -17,6 +17,9 @@ def profile(request):
             messages.success(request, 'Profile updated successfully')
         else:
             messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     form = UserProfileForm(instance=profile)
     orders=profile.orders.all()
